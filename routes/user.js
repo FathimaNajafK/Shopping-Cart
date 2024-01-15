e072c55b37895b70af72c004d31acf35e5d96d2f
FathimaNajafK <fathimanajafk@gmail.com>
2024-01-15 03:59:34 -0800

    view add

--- routes/user.js
+++ routes/user.js
@@ -20,7 +20,7 @@ router.get('/',async function(req, res, next) {
   console.log("user");
   let cartCount=null
   if(req.session.user){
-  let cartCount=await userHelpers.getCartCount(req.session.user._id)
+   cartCount=await userHelpers.getCartCount(req.session.user._id)
   }
   productHelpers.getAllProducts().then((products)=>{
    
